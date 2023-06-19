75aa9cac1a1da3611b7a690d5972ea604d16d0b1
unknown <angelorenz70@gmail.com>
2023-06-19 20:01:55 +0800

    progress

--- templates/documentation/types_migraine.php
+++ templates/documentation/types_migraine.php
@@ -1,56 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Intelligent System Models</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="{{url_for('static', filename='assets/img/neural-network.png')}}" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{url_for('static', filename='css/styles.css')}}" rel="stylesheet" />
-    </head>
-    <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand" href="#page-top"><img src="{{url_for('static', filename='assets/img/brain.png')}}" alt="..." /></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars ms-1"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">HOME</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                DOCUMENTATION
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                   <li><a class="dropdown-item" href="/energy_efficiency/doc">Energy Efficiency (LinR) Doc</a></li>
-                                    <li><a class="dropdown-item" href="/heart_disease/doc">Heart Disease (LogR) Doc</a></li>
-                                    <li><a class="dropdown-item" href="/types_migraine/doc">Types of Migraine (NN) Doc</a></li>
-                                    <li><a class="dropdown-item" href="/leukemia/doc">Leukemia (CNN) Doc</a></li>
-                                    <li><a class="dropdown-item" href="/reinforcementLearning/doc">Frozen Lake (RL) Doc</a></li>li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+{% extends 'documentation/base.php' %}
+
+{% block content %}
         <!-- Masthead-->
         <header class="masthead text-light" style="background-image: url('{{url_for('static', filename='assets/img/migraine_doc1.jpg')}}'); background-repeat: no-repeat; background-size: cover;">
             <div class="container">
-                <div class="masthead-subheading">LINEAR REGRESSION</div>
-                <div class="masthead-heading text-uppercase">APPLICATION: ENERGY EFFICIENCY</div>
+                <div class="masthead-subheading">NEURAL NETWORK</div>
+                <div class="masthead-heading text-uppercase">APPLICATION: TYPES OF MIGRAINE</div>
             </div>
         </header>
         <!-- Explanation-->
@@ -428,14 +383,4 @@
                 </div>
             </div>
         </div>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <!-- * *                               SB Forms JS                               * *-->
-        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-    </body>
-</html>
+{% endblock %}
